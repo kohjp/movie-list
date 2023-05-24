106a0f56ba6cce1746cecc09006131883175ae47
kohjp <rshnms12@gmail.com>
2023-05-24 14:15:05 +0900

    [feat] add about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,9 +4,32 @@ export default function About() {
   return (
     <div className="container">
       <Seo title="About" />
+      <div className="wrap">
+        <div>
+          {`영화 뭐 보지? 는 'TMDB' OPEN API를 통해 인기영화 리스트를 보여주는 웹
+        애플리케이션입니다.`}
+        </div>
+        <div>
+          <div>
+            <span>github : </span>
+            <a href="https://github.com/kohjp/movie-list">
+              https://github.com/kohjp/movie-list
+            </a>
+          </div>
+          <div>
+            <span>TMDB : </span>
+            <a href="https://www.themoviedb.org/">
+              https://www.themoviedb.org/
+            </a>
+          </div>
+        </div>
+      </div>
       <style jsx>{`
         .container {
           max-width: 100%;
+        }
+        .wrap {
+          padding: 20px;
         }
       `}</style>
     </div>
